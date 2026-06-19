242986d5782727530e1b37390da49b82ae733d05
EuGerdDev <EugenGerd@web.de>
2026-06-20 01:00:33 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,49 +1,258 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    Der Schüler hat <input id="points" type="number"> P. von <input id="maxpoints" type="number"> Punkten Erreicht. <br>
-    <span id="gradepoint"></span> Notenpunkte<br>
-    <button onclick="updateTable()">Ausrechnen</button> <br>
-    <table id="table">
-    <tr id="pointcontainer">
-        <td></td>
-    </tr>
-    <tr id="percantcontainer">
-        <td class="text">Prozent</td>
-<td id="percantborder1" class="number"><input id="percant1"  type="number" value="98"></td>
-<td id="percantborder2"  class="number"><input id="percant2"  type="number" value="94"></td>
-<td id="percantborder3"  class="number"><input id="percant3"  type="number" value="90"></td>
-<td id="percantborder4"  class="number"><input id="percant4"  type="number" value="85"></td>
-<td id="percantborder5"  class="number"><input id="percant5"  type="number" value="80"></td>
-<td id="percantborder6"  class="number"><input id="percant6"  type="number" value="75"></td>
-<td id="percantborder7"  class="number"><input id="percant7"  type="number" value="70"></td>
-<td id="percantborder8"  class="number"><input id="percant8"  type="number" value="65"></td>
-<td id="percantborder9"  class="number"><input id="percant9"  type="number" value="60"></td>
-<td id="percantborder10"  class="number"><input id="percant10"  type="number" value="55"></td>
-<td id="percantborder11"  class="number"><input id="percant11"  type="number" value="50"></td>
-<td id="percantborder12"  class="number"><input id="percant12"  type="number" value="45"></td>
-<td id="percantborder13"  class="number"><input id="percant13"  type="number" value="35"></td>
-<td id="percantborder14"  class="number"><input id="percant14"  type="number" value="25"></td>
-<td id="percantborder15"  class="number"><input id="percant15"  type="number" value="15"></td>
-<td id="percantborder16"  class="number"><input id="percant16"  type="number" value="0"></td>
-    </tr>
-    <tr id="gradepointcontainer">
+let gradepoints = [
+    {
+        name: "15",
+        percent: 0,
+        grade: "1+",
+        haveGrade: false
+    },
+    {
+        name: "14",
+        percent: 0,
+        grade: "1",
+        haveGrade: false
+    },
+    {
+        name: "13",
+        percent: 0,
+        grade: "1-",
+        haveGrade: false
+    },
+    {
+        name: "12",
+        percent: 0,
+        grade: "2+",
+        haveGrade: false
+    },
+    {
+        name: "11",
+        percent: 0,
+        grade: "2",
+        haveGrade: false
+    },
+    {
+        name: "10",
+        percent: 0,
+        grade: "2-",
+        haveGrade: false
+    },
+    {
+        name: "9",
+        percent: 0,
+        grade: "3+",
+        haveGrade: false
+    },
+    {
+        name: "8",
+        percent: 0,
+        grade: "3",
+        haveGrade: false
+    },
+    {
+        name: "7",
+        percent: 0,
+        grade: "3-",
+        haveGrade: false
+    },
+    {
+        name: "6",
+        percent: 0,
+        grade: "4+",
+        haveGrade: false
+    },
+    {
+        name: "5",
+        percent: 0,
+        grade: "4",
+        haveGrade: false
+    },
+    {
+        name: "4",
+        percent: 0,
+        grade: "4-",
+        haveGrade: false
+    },
+    {
+        name: "3",
+        percent: 0,
+        grade: "5+",
+        haveGrade: false
+    },
+    {
+        name: "2",
+        percent: 0,
+        grade: "5",
+        haveGrade: false
+    },
+    {
+        name: "1",
+        percent: 0,
+        grade: "5-",
+        haveGrade: false
+    },
+    {
+        name: "0",
+        percent: 0,
+        grade: "6",
+        haveGrade: false
+    },
+    
+]
+let percentages = [
+    {
+        name: "98",
+        value: 98,
+        points: 0
+    },
+    {
+        name: "89.6",
+        value: 94,
+        points: 0
+    },
+    {
+        name: "83.2",
+        value: 90,
+        points: 0
+    },
+    {
+        name: "76.8",
+        value: 85,
+        points: 0
+    },
+    {
+        name: "70.4",
+        value: 80,
+        points: 0
+    },
+    {
+        name: "64",
+        value: 75,
+        points: 64
+    },
+    {
+        name: "57.6",
+        value: 70,
+        points: 0
+    },
+    {
+        name: "51.2",
+        value: 65,
+        points: 0
+    },
+    {
+        name: "44.8",
+        value: 60,
+        points: 0
+    },
+    {
+        name: "38.4",
+        value: 55,
+        points: 0
+    },
+    {
+        name: "32",
+        value: 50,
+        points: 0
+    },
+    {
+        name: "25.6",
+        value: 45,
+        points: 0
+    },
+    {
+        name: "19.2",
+        value: 35,
+        points: 0
+    },
+    {
+        name: "12.8",
+        value: 25,
+        points: 0
+    },
+    {
+        name: "6.4",
+        value: 15,
+        points: 0
+    },
+    {
+        name: "0",
+        value: 0,
+        points: 0
+    },
+]
+let resultGradePoints;
+let percent;
+let grade;
+let amount = 0;
+function updateTable() {
+    let grades = 0;
+    let totalgradepoints = 0;
+    let averagegrade = 0;
+    amount += 1;
+    let points = Number(document.getElementById("points").value);
+    let maxpoints = Number(document.getElementById("maxpoints").value);
+    let pointcontainer = document.getElementById("pointcontainer");
+    let percantcontainer = document.getElementById("percantcontainer");
+    let gradepointcontainer = document.getElementById("gradepointcontainer");
+    let gradecontainer = document.getElementById("gradecontainer");
+    let gradeAmount = document.getElementById("gradeAmount");
+    pointcontainer.innerHTML = ``;
+    gradecontainer.innerHTML = ``;
+    gradepointcontainer.innerHTML = ``;
+    if(amount == 1){
+        gradeAmount.innerHTML = ``;
+        gradeAmount.innerHTML += `<td class="text">Anzahl der Schüler</td>`;
+    }
+    pointcontainer.innerHTML += `<td class="text">Rohpunkte</td>`;
+    gradepointcontainer.innerHTML += `<td class="text">Notenpunkte</td>`;
+    gradecontainer.innerHTML += `<td class="text">Noten</td>`;
+    
+    for (let index = 0; index < gradepoints.length; index++) {
+        let percant = Number(document.getElementById("percant" + (index + 1)).value);
+        percentages[index].value = percant;
+        let calculatepercant = percentages[index].value;
+        let grade = maxpoints * calculatepercant / 100;
+        let displaygrade = Math.round(grade);
+        percentages[index].points = displaygrade;
+        gradepoints[index].haveGrade = false;
+    }
+    for (let index = 0; index < gradepoints.length; index++) {
+        if(points >= percentages[index].points){
+            resultGradePoints = gradepoints[index].name;
+            gradepoints[index].haveGrade = true;
+            break;
+        }
+    }
+    for (let index = 0; index < gradepoints.length; index++) {
+        let percantborder = document.getElementById("percantborder" + (index + 1))
+        if(gradepoints[index].haveGrade){
+            pointcontainer.innerHTML += `<td style="border: 5px  rgb(89, 255, 47) inset;" class="number">${percentages[index].points}</td>`;
+            gradepointcontainer.innerHTML += `<td style="border: 5px  rgb(89, 255, 47) inset;" class="number">${gradepoints[index].name}</td>`;
+            gradecontainer.innerHTML += `<td style="border: 5px  rgb(89, 255, 47) inset;" class="number">${gradepoints[index].grade}</td>`;
+            percantborder.style.border = "5px rgb(89, 255, 47) inset";
+        }else{
+            pointcontainer.innerHTML += `<td class="number">${percentages[index].points}</td>`;
+            gradepointcontainer.innerHTML += `<td class="number">${gradepoints[index].name}</td>`;
+            gradecontainer.innerHTML += `<td class="number">${gradepoints[index].grade}</td>`;
+            percantborder.style.border = "none";
+        }
+        if(amount == 1){
+            gradeAmount.innerHTML += `<td class="number"><input type="number" id="${index + 1}"></td>`;
+        }
+        let gradeInput = Number(document.getElementById(index + 1).value);
+        if(!isNaN(gradeInput) && gradeInput > 0){
+            grades += gradeInput;
+            totalgradepoints += gradeInput * Number(gradepoints[index].name);
+        }
         
-    </tr>
-    <tr id="gradecontainer">
         
-    </tr>
-    <tr id="gradeAmount">
-        
-    </tr>
-    </table> <br>
-    Durchschnitt der Lerngruppe: ∅ = <span id="averagegrade"></span> NP
-    <script src="main.js"></script>
-</body>
-</html>
+    }
+    document.getElementById("gradepoint").textContent = resultGradePoints;
+
+    if(!isNaN(totalgradepoints / grades)){
+            averagegrade = totalgradepoints / grades;
+        }else{
+            averagegrade = 0;
+    }
+    document.getElementById("averagegrade").textContent =  averagegrade.toFixed(1);
+}
+updateTable();
