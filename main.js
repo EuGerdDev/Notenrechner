7a3ec4335ec6c098df54a87cfcb87c296d0d78bc
EuGerdDev <EugenGerd@web.de>
2026-06-29 16:18:31 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,357 +1,344 @@
 let gradepoints = [
     {
         name: "15",
-        percent: 0,
-        grade: "1+",
+        percent: 98,
+        points: 0,
+        grade: {
+            gr: "",
+            er: "1+"
+        },
         haveGrade: false,
         schoolleavingqualifications: [
             {name: "Abitur", index: 0},
             {name: "Berechtigung Übergang Gymnasiale Oberstufe", index: 3}
-        ]
+        ],
+        color: "rgb(0, 150, 0)"
     },
     {
         name: "14",
-        percent: 0,
-        grade: "1",
+        percent: 94,
+        points: 0,
+        grade: {
+            gr: "",
+            er: "1"
+        },
         haveGrade: false,
         schoolleavingqualifications: [
             {name: "Abitur", index: 0},
             {name: "Berechtigung Übergang Gymnasiale Oberstufe", index: 3}
-        ]
+        ],
+        color: "rgb(0, 150, 0)"
     },
     {
         name: "13",
-        percent: 0,
-        grade: "1-",
+        percent: 90,
+        points: 0,
+        grade: {
+            gr: "",
+            er: "1-"
+        },
         haveGrade: false,
         schoolleavingqualifications: [
             {name: "Abitur", index: 0},
             {name: "Berechtigung Übergang Gymnasiale Oberstufe", index: 3}
-        ]
+        ],
+        color: "rgb(0, 150, 0)"
     },
     {
         name: "12",
-        percent: 0,
-        grade: "2+",
+        percent: 85,
+        points: 0,
+        grade: {
+            gr: "1",
+            er: "2+"
+        },
         haveGrade: false,
         schoolleavingqualifications: [
             {name: "Abitur", index: 0},
             {name: "Berechtigung Übergang Gymnasiale Oberstufe", index: 3}
-        ]
+        ],
+        color: "rgb(62, 206, 26)"
     },
     {
         name: "11",
-        percent: 0,
-        grade: "2",
+        percent: 80,
+        points: 0,
+        grade: {
+            gr: "1",
+            er: "2"
+        },
         haveGrade: false,
         schoolleavingqualifications: [
             {name: "Abitur", index: 0},
             {name: "Berechtigung Übergang Gymnasiale Oberstufe", index: 3}
-        ]
+        ],
+        color: "rgb(62, 206, 26)"
     },
     {
         name: "10",
-        percent: 0,
-        grade: "2-",
+        percent: 75,
+        points: 0,
+        grade: {
+            gr: "1",
+            er: "2-"
+        },
         haveGrade: false,
         schoolleavingqualifications: [
             {name: "Abitur", index: 0},
             {name: "Berechtigung Übergang Gymnasiale Oberstufe", index: 3}
-        ]
+        ],
+        color: "rgb(62, 206, 26)"
     },
     {
         name: "9",
-        percent: 0,
-        grade: "3+",
+        percent: 70,
+        points: 0,
+        grade: {
+            gr: "2",
+            er: "3+"
+        },
         haveGrade: false,
         schoolleavingqualifications: [
             {name: "MSA", index: 1},
             {name: "Berechtigung Übergang Gymnasiale Oberstufe", index: 3}
-        ]
+        ],
+        color: "rgb(191, 228, 42)"
     },
     {
         name: "8",
-        percent: 0,
-        grade: "3",
+        percent: 65,
+        points: 0,
+        grade: {
+            gr: "2",
+            er: "3"
+        },
         haveGrade: false,
         schoolleavingqualifications: [
             {name: "MSA", index: 1},
             {name: "Berechtigung Übergang Gymnasiale Oberstufe", index: 3}
-        ]
+        ],
+        color: "yellow"
     },
     {
         name: "7",
-        percent: 0,
-        grade: "3-",
+        percent: 60,
+        points: 0,
+        grade: {
+            gr: "2",
+            er: "3-"
+        },
         haveGrade: false,
         schoolleavingqualifications: [
             {name: "MSA", index: 1},
             {name: "Berechtigung Übergang Gymnasiale Oberstufe", index: 3}
-        ]
+        ],
+        color: "yellow"
     },
     {
         name: "6",
-        percent: 0,
-        grade: "4+",
+        percent: 55,
+        points: 0,
+        grade: {
+            gr: "3",
+            er: "4+"
+        },
         haveGrade: false,
         schoolleavingqualifications: [
             {name: "MSA", index: 1},
-        ]
+        ],
+        color: "yellow"
     },
     {
         name: "5",
-        percent: 0,
-        grade: "4",
+        percent: 50,
+        points: 0,
+        grade: {
+            gr: "3",
+            er: "4"
+        },
         haveGrade: false,
         schoolleavingqualifications: [
             {name: "BBR/eBBR", index: 2},
             {name: "MSA", index: 1}
-        ]
+        ],
+        color: "orange"
     },
     {
         name: "4",
-        percent: 0,
-        grade: "4-",
+        percent: 45,
+        points: 0,
+        grade: {
+            gr: "4",
+            er: "4-"
+        },
         haveGrade: false,
         schoolleavingqualifications: [
             {name: "BBR/eBBR", index: 2},
             {name: "MSA", index: 1}
-        ]
+        ],
+        color: "orange"
     },
     {
         name: "3",
-        percent: 0,
-        grade: "5+",
+        percent: 35,
+        points: 0,
+        grade: {
+            gr: "4",
+            er: "5+"
+        },
         haveGrade: false,
         schoolleavingqualifications: [
             {name: "BBR/eBBR", index: 2},
             {name: "MSA", index: 1}
-        ]
+        ],
+        color: "orange"
     },
     {
         name: "2",
-        percent: 0,
-        grade: "5",
+        percent: 25,
+        points: 0,
+        grade: {
+            gr: "5",
+            er: "5"
+        },
         haveGrade: false,
         schoolleavingqualifications: [
             {name: "Kein Abschluss", index: 4}
-        ]
+        ],
+        color: "red"
     },
     {
         name: "1",
-        percent: 0,
-        grade: "5-",
+        percent: 15,
+        points: 0,
+        grade: {
+            gr: "5",
+            er: "5-"
+        },
         haveGrade: false,
         schoolleavingqualifications: [
             {name: "Kein Abschluss", index: 4}
-        ]
+        ],
+        color: "red"
     },
     {
         name: "0",
         percent: 0,
-        grade: "6",
+        points: 0,
+        grade: {
+            gr: "6",
+            er: "6"
+        },
         haveGrade: false,
         schoolleavingqualifications: [
             {name: "Kein Abschluss", index: 4}
-        ]
+        ],
+        color: "red"
     },
     
-]
-let percentages = [
-    {
-        name: "98",
-        value: 98,
-        points: 0
-    },
-    {
-        name: "89.6",
-        value: 94,
-        points: 0
-    },
-    {
-        name: "83.2",
-        value: 90,
-        points: 0
-    },
-    {
-        name: "76.8",
-        value: 85,
-        points: 0
-    },
-    {
-        name: "70.4",
-        value: 80,
-        points: 0
-    },
-    {
-        name: "64",
-        value: 75,
-        points: 64
-    },
-    {
-        name: "57.6",
-        value: 70,
-        points: 0
-    },
-    {
-        name: "51.2",
-        value: 65,
-        points: 0
-    },
-    {
-        name: "44.8",
-        value: 60,
-        points: 0
-    },
-    {
-        name: "38.4",
-        value: 55,
-        points: 0
-    },
-    {
-        name: "32",
-        value: 50,
-        points: 0
-    },
-    {
-        name: "25.6",
-        value: 45,
-        points: 0
-    },
-    {
-        name: "19.2",
-        value: 35,
-        points: 0
-    },
-    {
-        name: "12.8",
-        value: 25,
-        points: 0
-    },
-    {
-        name: "6.4",
-        value: 15,
-        points: 0
-    },
-    {
-        name: "0",
-        value: 0,
-        points: 0
-    },
 ]
 let schoolleavingqualifications = [
     {
         name: "Abitur",
-        id: "abi",
-        width: 440
+        id: "abi"
     },
     {
         name: "MSA",
-        id: "msa",
-        width: 440
+        id: "msa"
     },
     {
         name: "BBR/eBBR",
-        id: "bbr",
-        width: 218
+        id: "bbr"
     },
     {
         name: "Berechtigung Übergang Gymnasiale Oberstufe",
-        id: "bügo",
-        width: 670
+        id: "bügo"
     },
     {
         name: "Kein Abschluss",
-        id: "ka",
-        width: 218
+        id: "ka"
     },
 ]
-let resultGradePoints;
-let percent;
-let grade;
-let amount = 0;
-function updateTable() {
-    let grades = 0;
-    let totalgradepoints = 0;
-    let averagegrade = 0;
-    amount += 1;
-    let points = Number(document.getElementById("points").value);
-    let maxpoints = Number(document.getElementById("maxpoints").value);
-    let pointcontainer = document.getElementById("pointcontainer");
-    let percantcontainer = document.getElementById("percantcontainer");
-    let gradepointcontainer = document.getElementById("gradepointcontainer");
-    let gradecontainer = document.getElementById("gradecontainer");
-    let gradeAmount = document.getElementById("gradeAmount");
-    pointcontainer.innerHTML = ``;
-    gradecontainer.innerHTML = ``;
-    gradepointcontainer.innerHTML = ``;
-    if(amount == 1){
-        gradeAmount.innerHTML = ``;
-        gradeAmount.innerHTML += `<td class="text">Anzahl der Schüler</td>`;
-    }
-    pointcontainer.innerHTML += `<td class="text">Rohpunkte</td>`;
-    gradepointcontainer.innerHTML += `<td class="text">Notenpunkte</td>`;
-    gradecontainer.innerHTML += `<td class="text">Noten</td>`;
-    
+let calculations = 0;
+let resultGradePoints = 0;
+let resultGrade = 0;
+function initTable() {
+    generatePercentInputs();
+    calculatePoints();
+    renderGradePointRelativeFrequency();
+    calculateStudents();
+    renderTable();
+    renderText();
+}
+function calculatePoints() {
+    let maxpoints = document.getElementById("maxpoints").value;
     for (let index = 0; index < gradepoints.length; index++) {
-        let percant = Number(document.getElementById("percant" + (index + 1)).value);
-        percentages[index].value = percant;
-        let calculatepercant = percentages[index].value;
-        let grade = maxpoints * calculatepercant / 100;
-        let displaygrade = Math.round(grade);
-        percentages[index].points = displaygrade;
-        gradepoints[index].haveGrade = false;
-        if(amount == 1){
-            gradeAmount.innerHTML += `<td class="number"><input type="number" id="${index + 1}"></td>`;
-        }
-    }
-    for (let index = 0; index < gradepoints.length; index++) {
-        for (let iii = 0; iii < schoolleavingqualifications.length; iii++) {
-            document.getElementById(schoolleavingqualifications[iii].id).style.backgroundColor = "";
-        }
+        let percent = Number(document.getElementById("percent" + (index + 1)).value);
+        gradepoints[index].percent = percent;
+        let calculatepercent = gradepoints[index].percent;
+        let grade = maxpoints * calculatepercent / 100;
         
-        if(points >= percentages[index].points){
+        let displaygrade = Math.round(grade);
+        gradepoints[index].points = displaygrade;
+        gradepoints[index].haveGrade = false;
+    }
+}
+function generatePercentInputs() {
+    percentcontainer.innerHTML = `<td class="text">Prozent</td>`;
+    for (let index = 0; index < gradepoints.length; index++) {
+        percentcontainer.innerHTML += `<td id="percentborder${index + 1}" class="number"><input id="percent${index + 1}"  type="number" value="${gradepoints[index].percent}"></td>`;
+    }
+}
+function calculateGrade() {
+    let points = Number(document.getElementById("points").value);
+    for (let iii = 0; iii < schoolleavingqualifications.length; iii++) {
+        document.getElementById(schoolleavingqualifications[iii].id).style.backgroundColor = "";
+    }
+    for (let index = 0; index < gradepoints.length; index++) {
+        if(points >= gradepoints[index].points){
             resultGradePoints = gradepoints[index].name;
             gradepoints[index].haveGrade = true;
-            if(amount > 1)
             document.getElementById(index + 1).value =
             Number(document.getElementById(index + 1).value) + 1;
             
             for (let ii = 0; ii < gradepoints[index].schoolleavingqualifications.length; ii++) {
-                document.getElementById(schoolleavingqualifications[gradepoints[index].schoolleavingqualifications[ii].index].id).style.backgroundColor = "rgb(62, 206, 26)";
+                document.getElementById(schoolleavingqualifications[gradepoints[index].schoolleavingqualifications[ii].index].id).style.backgroundColor = gradepoints[index].color;
             }
+            resultGrade = gradepoints[index].grade.er;
             break;
         }
     }
+}
+function calculateStudents() {
+    let grades = 0;
+    let totalgradepoints = 0;
     for (let index = 0; index < gradepoints.length; index++) {
-        let percantborder = document.getElementById("percantborder" + (index + 1))
-        if(gradepoints[index].haveGrade){
-            pointcontainer.innerHTML += `<td style="background-color:  rgb(62, 206, 26);" class="number">${percentages[index].points}</td>`;
-            gradepointcontainer.innerHTML += `<td style="background-color: rgb(62, 206, 26);" class="number">${gradepoints[index].name}</td>`;
-            gradecontainer.innerHTML += `<td style="background-color: rgb(62, 206, 26);" class="number">${gradepoints[index].grade}</td>`;
-            percantborder.style.backgroundColor = "rgb(62, 206, 26)";
-        }else{
-            pointcontainer.innerHTML += `<td class="number">${percentages[index].points}</td>`;
-            gradepointcontainer.innerHTML += `<td class="number">${gradepoints[index].name}</td>`;
-            gradecontainer.innerHTML += `<td class="number">${gradepoints[index].grade}</td>`;
-            percantborder.style.backgroundColor = "";
-        }
-        
         let gradeInput = Number(document.getElementById(index + 1).value);
         if(!isNaN(gradeInput) && gradeInput > 0){
             grades += gradeInput;
             totalgradepoints += gradeInput * Number(gradepoints[index].name);
+        } 
+    }
+    for (let index = 0; index < gradepoints.length; index++) {
+        let gradeInput = Number(document.getElementById(index + 1).value);
+        if(grades >= 1){
+            let rh = gradeInput / grades * 100;
+            let displayrh = rh.toFixed(0);
+            document.getElementById(index + "rh").textContent = displayrh + " %";
+        }else{
+            document.getElementById(index + "rh").textContent = 0 + " %";
         }
-        
-        
+    } 
+    calculateGradePointAverage(grades, totalgradepoints);
+}
+function renderGradePointRelativeFrequency() {
+    gradeAmount.innerHTML = `<td class="text">Anzahl der Schüler</td>`;
+    for (let index = 0; index < gradepoints.length; index++) {
+        gradeAmount.innerHTML += `<td class="number"><input type="number" id="${index + 1}"><span id="${index}rh"></span></td>`;
     }
-    if(resultGradePoints == 1){
-        document.getElementById("gradepoint").textContent = resultGradePoints + " Notenpunkt";
-    }else{
-        document.getElementById("gradepoint").textContent = resultGradePoints + " Notenpunkte";
-    }
-    
-
+}
+function calculateGradePointAverage(grades, totalgradepoints) {
+    let averagegrade = 0;
     if(!isNaN(totalgradepoints / grades)){
             averagegrade = totalgradepoints / grades;
         }else{
@@ -359,4 +346,51 @@ function updateTable() {
     }
     document.getElementById("averagegrade").textContent =  averagegrade.toFixed(1);
 }
-updateTable();
+function renderTable() {
+    pointcontainer.innerHTML = `<td class="text">Rohpunkte</td>`;
+    gradepointcontainer.innerHTML = `<td class="text">Notenpunkte</td>`;
+    gradecontainer.innerHTML = `<td class="text">Noten GR Niveau</td>`;
+    ergradecontainer.innerHTML = `<td class="text">Noten ER Niveau</td>`;
+    for (let index = 0; index < gradepoints.length; index++) {
+        let percentborder = document.getElementById("percentborder" + (index + 1))
+        if(gradepoints[index].haveGrade){
+            pointcontainer.innerHTML += `<td style="background-color:  ${gradepoints[index].color};">${gradepoints[index].points}</td>`;
+            gradepointcontainer.innerHTML += `<td style="background-color:  ${gradepoints[index].color};" class="number">${gradepoints[index].name}</td>`;
+            gradecontainer.innerHTML += `<td style="background-color:  ${gradepoints[index].color};" class="number">${gradepoints[index].grade.gr}</td>`;
+            ergradecontainer.innerHTML += `<td style="background-color:  ${gradepoints[index].color};" class="number">${gradepoints[index].grade.er}</td>`;
+            percentborder.style.backgroundColor = gradepoints[index].color;
+        }else{
+            pointcontainer.innerHTML += `<td class="number">${gradepoints[index].points}</td>`;
+            gradepointcontainer.innerHTML += `<td class="number">${gradepoints[index].name}</td>`;
+            gradecontainer.innerHTML += `<td class="number">${gradepoints[index].grade.gr}</td>`;
+            ergradecontainer.innerHTML += `<td class="number">${gradepoints[index].grade.er}</td>`;
+            percentborder.style.backgroundColor = "";
+        }
+    }
+}
+function renderText(calculations) {
+    if(resultGradePoints == 1){
+        document.getElementById("gradepointtext").textContent = "Notenpunkt";
+    }else{
+        document.getElementById("gradepointtext").textContent = "Notenpunkte";
+    }
+    document.getElementById("gradetext").textContent = " Note ";
+    document.getElementById("gradepoint").textContent = resultGradePoints;
+    document.getElementById("grade").textContent = resultGrade;
+    document.getElementById("calculations").textContent = calculations;
+    document.getElementById("points").value = "";
+}
+function updateTable() {
+    calculations += 1;
+    calculatePoints();
+    calculateGrade();
+    calculateStudents();
+    renderTable();
+    renderText();
+}
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        updateTable();
+    }
+});
+initTable();
